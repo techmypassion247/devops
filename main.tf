898ed985db483e424a26778b04b9a119dc935efa
MyDevops <techmypassion247@gmail.com>
2025-02-23 14:46:48 +0530

    nginx install and deploy project

--- main.tf
+++ main.tf
@@ -13,6 +13,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0310b7d86993a61af"
+  associate_public_ip_address = true
 
   tags = {
     Name = "Terraform-Server"
