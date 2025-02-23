49574d6e1598914d7ccbb1a46e29847fcc097fdd
MyDevops <techmypassion247@gmail.com>
2025-02-23 23:12:32 +0530

    update sec_grp

--- Web_app_with_database/SecGrp.tf
+++ Web_app_with_database/SecGrp.tf
@@ -16,6 +16,10 @@ resource "aws_vpc_security_group_ingress_rule" "sshfromyIP" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+
+  lifecycle {
+    ignore_changes = all  # Prevent Terraform from modifying or recreating this resource
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
@@ -24,6 +28,10 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+
+  lifecycle {
+    ignore_changes = all  # Prevent Terraform from modifying or recreating this resource
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
@@ -32,16 +40,28 @@ resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
   from_port         = 3306
   ip_protocol       = "tcp"
   to_port           = 3306
+
+  lifecycle {
+    ignore_changes = all  # Prevent Terraform from modifying or recreating this resource
+  }
 }
 
 resource "aws_vpc_security_group_egress_rule" "allowAllOutbound_ipv4" {
   security_group_id = aws_security_group.dove-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+
+  lifecycle {
+    ignore_changes = all  # Prevent Terraform from modifying or recreating this resource
+  }
 }
 
 resource "aws_vpc_security_group_egress_rule" "allowAllOutbound_ipv6" {
   security_group_id = aws_security_group.dove-sg.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+
+  lifecycle {
+    ignore_changes = all  # Prevent Terraform from modifying or recreating this resource
+  }
 }
